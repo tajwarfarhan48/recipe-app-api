559646b0a270357627f78268d9bf8215948ab2b4
tajwarfarhan48 <tajwarfarhan48@gmail.com>
2022-05-27 18:59:52 +0600

    Installed Psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add x and y and return the result.
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract y from x and return the result.
+    """
+
+    return x - y
